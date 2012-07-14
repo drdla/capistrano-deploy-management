35da686fb7581d4c398e07451ceebb8120da7aa7
drdla <dominik.rodler@googlemail.com>
2012-07-14 16:24:11 +0300

    changed deploy_to to current_path

--- lib/capistrano-deploy/passenger.rb
+++ lib/capistrano-deploy/passenger.rb
@@ -5,7 +5,7 @@ module CapistranoDeploy
         namespace :passenger do
           desc 'Restart passenger'
           task :restart, :roles => :app, :except => {:no_release => true} do
-            run "touch #{deploy_to}/tmp/restart.txt"
+            run "touch #{current_path}/tmp/restart.txt"
           end
         end
 
