b4a9471fd4f39404a3546caef8a84bd886a67dc6
drdla <dominik.rodler@googlemail.com>
2012-07-14 16:23:23 +0300

    changed deploy_to to current_path

--- lib/capistrano-deploy/git.rb
+++ lib/capistrano-deploy/git.rb
@@ -9,7 +9,7 @@ module CapistranoDeploy
         set :branch, 'master'
         set :enable_submodules, false
 
-        set(:current_revision) { capture("cd #{deploy_to} && git rev-parse HEAD").chomp }
+        set(:current_revision) { capture("cd #{current_path} && git rev-parse HEAD").chomp }
 
         set :local_branch do
           `git symbolic-ref HEAD 2> /dev/null`.strip.sub('refs/heads/', '')
@@ -18,14 +18,14 @@ module CapistranoDeploy
         namespace :deploy do
           desc 'Setup'
           task :setup, :except => {:no_release => true} do
-            run "mkdir -p `dirname #{deploy_to}` && git clone --no-checkout #{repository} #{deploy_to}"
+            run "mkdir -p `dirname #{current_path}` && git clone --no-checkout #{repository} #{deploy_to}"
             update
           end
 
           desc 'Update the deployed code'
           task :update, :except => {:no_release => true} do
             commit = ENV['COMMIT'] || "origin/#{branch}"
-            command = ["cd #{deploy_to}", 'git fetch origin', "git reset --hard #{commit}"]
+            command = ["cd #{current_path}", 'git fetch origin', "git reset --hard #{commit}"]
             command += ['git submodule init', 'git submodule -q sync', 'git submodule -q update'] if enable_submodules
             run command.join(' && ')
           end
