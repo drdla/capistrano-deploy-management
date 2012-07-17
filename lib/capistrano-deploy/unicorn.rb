f236c630bafd25ba27677d73153821a99b5c0444
Andy Gildehaus <agildehaus@gmail.com>
2012-07-17 09:59:10 +0200

    Fixed other tasks as well.

--- lib/capistrano-deploy/unicorn.rb
+++ lib/capistrano-deploy/unicorn.rb
@@ -2,22 +2,22 @@ module CapistranoDeploy
   module Unicorn
     def self.load_into(configuration)
       configuration.load do
-        set(:unicorn_pid) { "`cat #{deploy_to}/shared/pids/unicorn.pid`" }
+        set(:unicorn_pidfile) { "#{deploy_to}/shared/pids/unicorn.pid" }
 
         namespace :unicorn do
           desc 'Reload unicorn'
           task :reload, :roles => :app, :except => {:no_release => true} do
-            run "test -s #{unicorn_pid} && kill -s USR2 #{unicorn_pid}"
+            run "test -s #{unicorn_pidfile} && kill -s USR2 `cat #{unicorn_pid}`"
           end
 
           desc 'Stop unicorn'
           task :stop, :roles => :app, :except => {:no_release => true} do
-            run "kill -QUIT #{unicorn_pid}"
+            run "kill -s QUIT `cat #{unicorn_pidfile}`"
           end
 
           desc 'Reexecute unicorn'
           task :reexec, :roles => :app, :except => {:no_release => true} do
-            run "kill -USR2 #{unicorn_pid}"
+            run "kill -s USR2 `cat #{unicorn_pidfile}`"
           end
         end
       end
