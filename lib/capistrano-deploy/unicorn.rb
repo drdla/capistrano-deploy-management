a2308e9934111a009932afc2ed2760771aa6d4d9
Andy Gildehaus <agildehaus@gmail.com>
2012-07-18 20:37:25 +0200

    Added error messages.

--- lib/capistrano-deploy/unicorn.rb
+++ lib/capistrano-deploy/unicorn.rb
@@ -7,17 +7,17 @@ module CapistranoDeploy
         namespace :unicorn do
           desc 'Reload unicorn'
           task :reload, :roles => :app, :except => {:no_release => true} do
-            run "test -s #{unicorn_pidfile} && kill -s USR2 `cat #{unicorn_pidfile}`"
+            run "test -s #{unicorn_pidfile} && kill -s USR2 `cat #{unicorn_pidfile}` || echo 'pidfile does not exist'"
           end
 
           desc 'Stop unicorn'
           task :stop, :roles => :app, :except => {:no_release => true} do
-            run "kill -s QUIT `cat #{unicorn_pidfile}`"
+            run "test -s #{unicorn_pidfile} && kill -s QUIT `cat #{unicorn_pidfile}` || echo 'pidfile does not exist'"
           end
 
           desc 'Reexecute unicorn'
           task :reexec, :roles => :app, :except => {:no_release => true} do
-            run "kill -s USR2 `cat #{unicorn_pidfile}`"
+            run "test -s #{unicorn_pidfile} && kill -s USR2 `cat #{unicorn_pidfile}` || echo 'pidfile does not exist'"
           end
         end
       end
