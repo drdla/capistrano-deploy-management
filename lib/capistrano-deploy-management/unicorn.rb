795b9df38c5409aabed9d96aaaf463bd003830c6
Dominik Rodler <dominik.rodler@googlemail.com>
2012-10-21 10:44:40 +0200

    attempts to fix broken stopping and restarting of unicorn

--- lib/capistrano-deploy-management/unicorn.rb
+++ lib/capistrano-deploy-management/unicorn.rb
@@ -4,16 +4,18 @@ module CapistranoDeployManagement
       configuration.load do
 
         set(:unicorn_config)  { "#{current_path}/config/unicorn.rb" }
-        set(:unicorn_pidfile) { "#{deploy_to}/shared/pids/unicorn.pid" }
-        set(:unicorn_pid)     { "cat #{deploy_to}/shared/pids/unicorn.pid" }
+        # FIXME: Restarting fails, because PID does not exist -> symlink already points to new, empty folder
+        # set(:unicorn_pidfile) { "#{deploy_to}/shared/pids/unicorn.pid" }
+        # set(:unicorn_pid)     { "cat #{deploy_to}/shared/pids/unicorn.pid" }
+        set(:unicorn_pidfile) { "#{previous_release}/tmp/pids/unicorn.pid" }
+        set(:unicorn_pid)     { "cat #{previous_release}/tmp/pids/unicorn.pid" }
 
         namespace :unicorn do
           desc 'Restart unicorn.'
           task :restart, :roles => :app do
-            # FIXME: PID does not exist, thus restarting fails
-            # run "#{try_sudo} kill -s USR2 $(#{unicorn_pid})"
-            unicorn.stop
-            unicorn.start
+            run "if [ -f #{unicorn_pidfile} ] && [ -e $(#{unicorn_pid}) ]; then #{try_sudo} kill -s USR2 $(#{unicorn_pid}); else cd #{current_path} && bundle exec unicorn -c #{unicorn_config} -E #{rails_env} -D; fi"
+            # unicorn.stop
+            # unicorn.start
           end
 
           desc 'Start unicorn.'
@@ -23,7 +25,7 @@ module CapistranoDeployManagement
 
           desc 'Stop unicorn.'
           task :stop, :roles => :app do
-            run "cd #{current_path} && test -s #{unicorn_pidfile} && #{try_sudo} kill $(#{unicorn_pid}) || echo 'Unicorn not running. Nothing to kill.'"
+            run "cd #{current_path} && test -s #{unicorn_pidfile} && #{try_sudo} kill -QUIT $(#{unicorn_pid}) || echo 'Unicorn not running. Nothing to kill.'"
           end
         end
 
